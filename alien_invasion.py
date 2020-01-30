0d86a155961137b15a54f27f16c3ef12e745a5b9
Deon Choi <deon.choi@gmail.com>
2020-01-30 11:05:42 -0800

    added ship image

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,14 @@
 import sys
-
 import pygame
+from settings import Settings
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((1200,800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (self.settings.bg_color)
         
 
     def run_game(self):
@@ -14,6 +16,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            self.screen.fill(self.bg_color)
+
             pygame.display.flip()
 
             
