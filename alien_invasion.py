1370d4d635584da82f509a0dd17e52c1ce3bba13
Deon Choi <deon.choi@gmail.com>
2020-01-30 18:57:55 -0800

    aliens move left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,6 +28,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
             for bullet in self.bullets.copy():
@@ -56,6 +57,23 @@ class AlienInvasion:
         alien.rect.x = alien.x
         alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
+
+    def _update_aliens(self):
+        self.aliens.update()
+
+        self._check_fleet_edges()
+        self.aliens.update()
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+        
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _check_events(self):
         for event in pygame.event.get():
